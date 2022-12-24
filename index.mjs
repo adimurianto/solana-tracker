c888d7090e95d8346081be6e72a8a805bb96531c
AM Codes <amccodes@gmail.com>
2022-12-24 14:44:29 +0700

    Change currency using IDR

--- index.mjs
+++ index.mjs
@@ -5,26 +5,26 @@ import cron from 'node-cron';
 
 const config = {
   method: 'get',
-  url: 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?convert=USD&slug=solana',
+  url: 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?convert=IDR&slug=solana',
   headers: { 
     'X-CMC_PRO_API_KEY': token_coinmarket
   }
 };
 
-cron.schedule('*/5 * * * *', async () => {
+cron.schedule('* * * * * *', async () => {
   try {
     const writeApi = new InfluxDB({url, token}).getWriteApi(org, bucket, 'ns')
     const response = await axios.request(config);
 
     const point1 = new Point('solana')
-      .tag('currency', 'usd')
-      .floatField('price', response.data['data']['5426']['quote']['USD']['price'])
+      .tag('currency', 'idr')
+      .floatField('price', response.data['data']['5426']['quote']['IDR']['price'])
       .timestamp(new Date)
     writeApi.writePoint(point1)
     
     await writeApi.close()
 
-    console.log('Solana price is $'+response.data['data']['5426']['quote']['USD']['price']+' success to write at ' + new Date());
+    console.log('Solana price is $'+response.data['data']['5426']['quote']['IDR']['price']+' success to write at ' + new Date());
   } catch (error) {
     console.error(error);
   }
